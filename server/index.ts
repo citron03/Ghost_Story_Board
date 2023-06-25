2116230c5896dc833835aee4f2b58725bd1de375
citron03 <cchan0617@gmail.com>
2023-06-25 15:48:19 +0900

    test 라우터 생성 (post, tag 생성 및 댓글 작성 기능 테스트)

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import type { ErrorRequestHandler } from "express";
 
+import testRouter from "./src/router/test";
 import { AppDataSource } from "./src/data-source";
-import { Post, Comment, Tag } from "./src/entity";
-
 import logger from "./src/logger";
 
 // typeorm DB
@@ -17,25 +16,15 @@ AppDataSource.initialize()
 const app = express();
 
 app.use("/static", express.static("static"));
+app.use(express.json()); // use body
 
 const port = 8080;
+
+app.use("/test", testRouter);
 
 app.get("/", (req, res, next) => {
   logger.info("서버 정상 작동!");
   res.status(200).json({ message: "server is running...." });
-});
-
-app.get("/test/post", async (req, res, next) => {
-  const post = new Post();
-  post.title = "테스트 포스팅";
-  post.content = "글 내용입니다.";
-  post.writer = "작성자1";
-  try {
-    const result = await AppDataSource.manager.save(post);
-    res.status(200).json({ message: `${result.id} 글 작성 성공!` });
-  } catch (err) {
-    next(err);
-  }
 });
 
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
