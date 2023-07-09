8d8fca5fec4df2705ee151fa8b05b2f627a0c897
citron03 <cchan0617@gmail.com>
2023-07-09 16:01:22 +0900

    style 일부 수정

--- client/components/TagCard.tsx
+++ client/components/TagCard.tsx
@@ -1,5 +1,6 @@
 import { Tag } from "@chakra-ui/react";
 import type { Tag as TagType } from "@/types/postType";
+import styles from "./TagCard.module.css";
 
 export default function TagCard({ name }: Partial<TagType>) {
   return (
@@ -8,6 +9,7 @@ export default function TagCard({ name }: Partial<TagType>) {
       variant="solid"
       colorScheme="teal"
       onClick={() => alert(`${name} 태그 클릭!`)}
+      className={styles.container}
     >
       {name}
     </Tag>
