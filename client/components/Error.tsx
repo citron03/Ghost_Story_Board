2514bdeefe3471379eef24c64b2a192c2d46442c
citron03 <cchan0617@gmail.com>
2023-07-15 00:31:36 +0900

    css 수정 (에러 및 post card 모바일 환경)

--- client/components/Error.tsx
+++ client/components/Error.tsx
@@ -1,12 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { WarningIcon } from "@chakra-ui/icons";
 import { FallbackProps } from "react-error-boundary";
 
 export default function Error({ error, resetErrorBoundary }: FallbackProps) {
   return (
-    <Box>
-      <h1>ERROR!</h1>
-      <p> 에러: {error.message} </p>
-      <button onClick={() => resetErrorBoundary()}> 다시 시도 </button>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Heading marginBottom="2.5">ERROR!</Heading>
+      <Box display="flex" flexDirection="row" alignItems="center">
+        <WarningIcon w={8} h={8} color="red.500" margin="2" />
+        <Text fontSize="lg"> 에러: {error.message} </Text>
+      </Box>
+      <Button colorScheme="red" onClick={() => resetErrorBoundary()}>
+        다시 시도
+      </Button>
     </Box>
   );
 }
