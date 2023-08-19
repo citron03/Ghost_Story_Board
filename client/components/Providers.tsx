9875540eb42263a48edc397e0175297b3f1628fc
citron03 <cchan0617@gmail.com>
2023-08-19 10:53:41 +0900

    add @tanstack/react-query-devtools

--- client/components/Providers.tsx
+++ client/components/Providers.tsx
@@ -1,13 +1,17 @@
 import { Metadata } from "next";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
