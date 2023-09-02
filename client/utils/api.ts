7513ac1ae04c0b24a52a8581514c00332fb9cf78
citron03 <cchan0617@gmail.com>
2023-09-02 12:26:24 +0900

    deploy setting

--- client/utils/api.ts
+++ client/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create();
-api.defaults.baseURL = "http://127.0.0.1:8080";
+// api.defaults.baseURL = "http://127.0.0.1:8080";
+api.defaults.baseURL = "https://gsb-testing.kro.kr";
 
 export default api;
